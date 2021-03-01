33434c3efecae712f7f740f232effea53d9a9dc7
dave <noreply@thecoderscorner.com>
2021-03-01 17:03:08 +0000

    Minor fixes for better backward compatibility 1.7 to 2.0

--- src/GfxMenuConfig.h
+++ src/GfxMenuConfig.h
@@ -2,9 +2,9 @@
 #ifndef TCMENU_LEGACY_GFXMENUCONFIG_H
 #define TCMENU_LEGACY_GFXMENUCONFIG_H
 
-#include <graphics/GfxMenuConfig.h>
+#include "graphics/GfxMenuConfig.h"
 
 // now include the namespace to ensure everything sees it exactly as before.
-using namespace txgfx;
+using namespace tcgfx;
 
 #endif //TCMENU_LEGACY_GFXMENUCONFIG_H
