284bd3a774115af19cb9bd56bb08933b51a92c2d
Dave Cherry <dave@nutricherry.com>
2021-09-19 10:13:13 +0100

    Release V2.2.3

--- src/tcMenuVersion.h
+++ src/tcMenuVersion.h
@@ -17,7 +17,7 @@ namespace tccore {
 // here we define the version as both a string and separate field
 #define TCMENU_MAJOR 2
 #define TCMENU_MINOR 2
-#define TCMENU_PATCH 2
+#define TCMENU_PATCH 3
 
 /**
  * A helper to generate the major minor version numbers used in the protocol
