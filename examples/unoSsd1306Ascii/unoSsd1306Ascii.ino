040f0b29a8173a7a8984f8bb7123590db242f6b8
Dave Cherry <dave@nutricherry.com>
2022-10-01 17:25:32 +0100

    remove debug statements.

--- examples/unoSsd1306Ascii/unoSsd1306Ascii.ino
+++ examples/unoSsd1306Ascii/unoSsd1306Ascii.ino
@@ -6,6 +6,10 @@
 
 #include <TaskManagerIO.h>
 
+//
+// The below display configuration is more or less copied from the example, you can adjust as needed
+//
+
 // 0X3C+SA0 - 0x3C or 0x3D
 #define I2C_ADDRESS 0x3C
 #define RST_PIN -1
@@ -13,11 +17,15 @@ SSD1306AsciiAvrI2c gfx;
 
 void setup() {
     Serial.begin(115200);
+
+    // during setup and before calling setupMenu() you need to initialise the display
 #if RST_PIN >= 0
     gfx.begin(&Adafruit128x64, I2C_ADDRESS, RST_PIN);
 #else // RST_PIN >= 0
     gfx.begin(&Adafruit128x64, I2C_ADDRESS);
 #endif // RST_PIN >= 0
+
+    menuIntList.setNumberOfRows(10);
 
     setupMenu();
 }
@@ -26,19 +34,28 @@ void loop() {
     taskManager.runLoop();
 }
 
+// here we declare a program memory string for later use.
+const char pgmListItemName[] PROGMEM = "My List";
 
+//
+// This callback is called when the list item needs data, it is called for the items name, values and upon item press
 // see tcMenu list documentation on thecoderscorner.com
+//
 int CALLBACK_FUNCTION fnIntListRtCall(RuntimeMenuItem* item, uint8_t row, RenderFnMode mode, char* buffer, int bufferSize) {
    switch(mode) {
     case RENDERFN_INVOKE:
-        // TODO - your code to invoke goes here - row is the index of the item
+        Serial.print("Select "); Serial.println(row);
         return true;
     case RENDERFN_NAME:
-        // TODO - each row has it's own name - 0xff is the parent item
-        ltoaClrBuff(buffer, row, 3, NOT_PADDED, bufferSize);
+        // TODO - each row has it's own name - LIST_PARENT_ITEM_POS is the parent item
+        if(row == LIST_PARENT_ITEM_POS) {
+            strcpy_P(buffer, pgmListItemName);
+        } else {
+            ltoaClrBuff(buffer, row, 2, NOT_PADDED, bufferSize);
+        }
         return true;
     case RENDERFN_VALUE:
-        // TODO - each row can has its own value - 0xff is the parent item
+        // TODO - each row can has its own value - LIST_PARENT_ITEM_POS is the parent item
         buffer[0] = 'V'; buffer[1]=0;
         fastltoa(buffer, row, 3, NOT_PADDED, bufferSize);
         return true;
