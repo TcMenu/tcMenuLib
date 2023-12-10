4600a74fd8ed8936289cd4fb1d01a4c3a2429680
Roman Bekas <bekas.roman@gmail.com>
2023-12-10 03:21:57 +0200

    fixed ukrainian language missing problem

---
+++ src/lang/language_uk.h
@@ -0,0 +1,54 @@
+/**
+ * @file language_uk.h
+ * @brief Ніколи не включайте безпосередньо; містить англійський текст для тексту tcMenu, керованого TC_LOCALE, це значення за замовчуванням (EN).
+ */
+
+//
+// Підтримка діалогів, зауважте: будь ласка, зберігайте ok, cancel, close та accept у нижньому регістрі
+//
+
+#define TXT_DIALOG_OK "ок"
+#define TXT_DIALOG_CANCEL "скасувати"
+#define TXT_DIALOG_CLOSE "закрити"
+#define TXT_DIALOG_ACCEPT "прийняти"
+
+//
+// Варіанти назв булевих значень
+//
+
+#define TXT_BOOL_ON_TEXT "УВІМК"
+#define TXT_BOOL_OFF_TEXT "ВИМК"
+#define TXT_BOOL_YES_TEXT "ТАК"
+#define TXT_BOOL_NO_TEXT "НІ"
+#define TXT_BOOL_TRUE_TEXT "ПРАВДА"
+#define TXT_BOOL_FALSE_TEXT "ХИБА"
+
+//
+// Опції тексту сенсорного екрану IOA
+//
+
+#define TXT_TOUCH_CONFIGURE "Налаштування сенсорного екрану"
+#define TXT_TOUCH_CONFIGURE_2ND "Вибрані області сенсорного екрану"
+
+//
+// Підключення до віддаленого з'єднання - спарювання
+//
+
+#define TXT_PAIRING_TEXT "Очікування спарювання"
+
+//
+// Текст діалогу RemoteMenuItem для віддалених підключень та аутентифікації
+//
+
+#define TXT_CLOSE_CONNECTION "Закрити з'єднання"
+#define TXT_AUTH_REMOVE "Видалити"
+#define TXT_AUTH_REMOVE_ALL_KEYS "Видалити ВСІ ключі?"
+#define TXT_AUTH_EMPTY_KEY "Порожній ключ"
+
+//
+// Безпечний пункт меню спливаючого вікна
+//
+
+#define TXT_SECURE_MENU_PROCEED "Продовжити"
+#define TXT_SECURE_MENU_CANCEL "Скасувати"
+#define TXT_SECURE_PIN_WRONG "Неправильний PIN"
