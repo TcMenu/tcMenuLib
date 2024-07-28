6bc4d84f04f99ed279aaa544480c7fa09bd044a7
dave <dave@nutricherry.com>
2024-07-28 10:48:50 +0100

    #222 one button support working

--- src/extras/TcOneButtonHandler.h
+++ src/extras/TcOneButtonHandler.h
@@ -9,8 +9,8 @@
  * Support for a menu with a single button, it will add a button on the required pin, and listen for changes
  * With the following:
  *   Short press: increment by 1
- *   Double press (when not editing): select item
- *   Hold down: go back navigation
+ *   Double press (when not editing): back navigation
+ *   Hold down: select
  */
 class TcOneButtonHandler : public SwitchListener {
 private:
@@ -31,14 +31,20 @@ public:
 
     void onReleased(pinid_t pin, bool held) override {
         if(held && pin == buttonPin) {
-            menuMgr.performDirectionMove(true);
+            menuMgr.onMenuSelect(false);
             lastPress = 0;
         } else {
             if((millis() - lastPress) < doubleClickThreshold) {
-                menuMgr.onMenuSelect(held);
+                menuMgr.performDirectionMove(true);
                 lastPress = 0;
             } else {
-                switches.getEncoder()->increment(1);
+                auto enc = switches.getEncoder();
+                if(enc->getCurrentReading() == enc->getMaximumValue()) {
+                    enc->setCurrentReading(0);
+                    enc->runCallback(enc->getCurrentReading());
+                } else {
+                    enc->increment(1);
+                }
                 lastPress = millis();
             }
         }
